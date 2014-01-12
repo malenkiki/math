d3c5448c59353d75a8d0f7b97c1d6ffc2b9bc371
malenkiki <petit.michel@gmail.com>
2014-01-13 00:45:30 +0100

    Start adding Matrix basis

---
+++ src/Malenki/Math/Matrix.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Copyright (c) 2014 Michel Petit <petit.michel@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+namespace Malenki\Math;
+
+
+class Matrix
+{
+    protected $arr = array();
+    protected $size = null;
+
+
+    public function __construct($int_cols, $int_rows)
+    {
+        if(!is_numeric($int_cols) || !is_numeric($int_rows))
+        {
+            throw new \InvalidArgumentException('number of cols and rows must be integers.');
+        }
+
+        $int_cols = (integer) $int_cols;
+        $int_rows = (integer) $int_rows;
+
+        if($int_cols <= 0 || $int_rows <= 0)
+        {
+            throw new \InvalidArgumentException('Number of cols and rows must be positive not null integers.');
+        }
+
+        $this->size = new \stdClass();
+        $this->size->cols = $int_cols;
+        $this->size->rows = $int_rows;
+    }
+
+    public function addRow($arr_row)
+    {
+    }
+
+
+
+    public function addColunm($arr_col)
+    {
+    }
+}
