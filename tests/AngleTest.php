2f9628e0952168e55430db7d9469f188ad057e40
Michel Petit <petit.michel@gmail.com>
2014-01-27 19:20:36 +0100

    Some other unit testing about angles

--- tests/AngleTest.php
+++ tests/AngleTest.php
@@ -70,5 +70,57 @@ class AngleTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(2 * pi(), $a->rad);
         $this->assertEquals(1, $a->turn);
         $this->assertEquals(400, $a->gon);
+        
+        $a = new Angle(720, Angle::TYPE_DEG);
+        $this->assertEquals(720, $a->deg);
+        $this->assertEquals(4 * pi(), $a->rad);
+        $this->assertEquals(2, $a->turn);
+        $this->assertEquals(800, $a->gon);
+    }
+    
+    
+    public function testCreateFromRadiansToGetOthers()
+    {
+        $a = new Angle(0, Angle::TYPE_RAD);
+        $this->assertEquals(0, $a->deg);
+        $this->assertEquals(0, $a->rad);
+        $this->assertEquals(0, $a->turn);
+        $this->assertEquals(0, $a->gon);
+
+        $a = new Angle(pi() / 4, Angle::TYPE_RAD);
+        $this->assertEquals(45, $a->deg);
+        $this->assertEquals(pi() / 4, $a->rad);
+        $this->assertEquals(1 / 8, $a->turn);
+        $this->assertEquals(50, $a->gon);
+        
+        $a = new Angle(pi() / 2, Angle::TYPE_RAD);
+        $this->assertEquals(90, $a->deg);
+        $this->assertEquals(pi() / 2, $a->rad);
+        $this->assertEquals(1 / 4, $a->turn);
+        $this->assertEquals(100, $a->gon);
+        
+        $a = new Angle(pi(), Angle::TYPE_RAD);
+        $this->assertEquals(180, $a->deg);
+        $this->assertEquals(pi(), $a->rad);
+        $this->assertEquals(1 / 2, $a->turn);
+        $this->assertEquals(200, $a->gon);
+        
+        $a = new Angle(3 * pi() / 2, Angle::TYPE_RAD);
+        $this->assertEquals(270, $a->deg);
+        $this->assertEquals(3 * pi() / 2, $a->rad);
+        $this->assertEquals(3/4, $a->turn);
+        $this->assertEquals(300, $a->gon);
+         
+        $a = new Angle(2 * pi(), Angle::TYPE_RAD);
+        $this->assertEquals(360, $a->deg);
+        $this->assertEquals(2 * pi(), $a->rad);
+        $this->assertEquals(1, $a->turn);
+        $this->assertEquals(400, $a->gon);
+        
+        $a = new Angle(4 * pi(), Angle::TYPE_RAD);
+        $this->assertEquals(720, $a->deg);
+        $this->assertEquals(4 * pi(), $a->rad);
+        $this->assertEquals(2, $a->turn);
+        $this->assertEquals(800, $a->gon);
     }
 }
