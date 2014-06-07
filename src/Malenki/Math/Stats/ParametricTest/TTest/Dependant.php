4081b7b08fe9a6ca23d74118976e693a072777d8
Michel Petit <petit.michel@gmail.com>
2014-06-08 01:55:59 +0200

    Stats Parametric: new hier for t-Tests

--- src/Malenki/Math/Stats/ParametricTest/TTest/Dependant.php
+++ src/Malenki/Math/Stats/ParametricTest/TTest/Dependant.php
@@ -22,10 +22,10 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-namespace Malenki\Math\Stats\ParametricTest;
+namespace Malenki\Math\Stats\ParametricTest\TTest;
 use \Malenki\Math\Stats\Stats;
 
-class DependantTTestOfStudent implements \Countable
+class Dependant implements \Countable
 {
     protected $arr_samples = array();
     protected $int_count = null;
@@ -33,22 +33,20 @@ class DependantTTestOfStudent implements \Countable
     protected $float_sigma = null;
     protected $float_t = null;
 
-
     public function __get($name)
     {
-        if(in_array($name, array('count', 'dof', 'sigma', 't'))){
+        if (in_array($name, array('count', 'dof', 'sigma', 't'))) {
             return $this->$name();
-        } elseif(in_array($name, array('degrees_of_freedom', 'df'))){
+        } elseif (in_array($name, array('degrees_of_freedom', 'df'))) {
             return $this->degreesOfFreedom();
         }
     }
-    
-    
+
     public function __set($name, $value)
     {
         if(
             in_array(
-                $name, 
+                $name,
                 array(
                     'sample_one',
                     'sample_two',
@@ -60,16 +58,15 @@ class DependantTTestOfStudent implements \Countable
             )
         )
         {
-            if(is_array($value)){
+            if (is_array($value)) {
                 $value = new Stats($value);
-            } elseif(!($value instanceof Stats))
-            {
+            } elseif (!($value instanceof Stats)) {
                 throw new \InvalidArgumentException(
                     'Added sample to Dependant t-test of Student must be array or Stats instance'
                 );
             }
 
-            if(preg_match('/_(1|one|a)$/',$name)){
+            if (preg_match('/_(1|one|a)$/',$name)) {
                 $this->arr_samples[0] = $value;
             } else {
                 $this->arr_samples[1] = $value;
@@ -79,27 +76,24 @@ class DependantTTestOfStudent implements \Countable
         }
     }
 
-
-
     public function add($s)
     {
-        if(count($this->arr_samples) == 2){
+        if (count($this->arr_samples) == 2) {
             throw new \RuntimeException(
                 'Student’s t-Test For Dependant samples does not use more than two samples!'
             );
         }
 
-        if(is_array($s)){
+        if (is_array($s)) {
             $s = new Stats($s);
-        } elseif(!($s instanceof Stats))
-        {
+        } elseif (!($s instanceof Stats)) {
             throw new \InvalidArgumentException(
                 'Added sample to Student’s t-Test For Dependant samples must be array or Stats instance'
             );
         }
 
-        if(count($this->arr_samples) == 1){
-            if(count($s) != count($this->arr_samples[0])){
+        if (count($this->arr_samples) == 1) {
+            if (count($s) != count($this->arr_samples[0])) {
                 throw new \RuntimeException(
                     'Student’s t-Test For Dependant samples must use two sample having the same size.'
                 );
@@ -126,7 +120,6 @@ class DependantTTestOfStudent implements \Countable
         $this->int_count = null;
     }
 
-
     public function count()
     {
         $this->compute();
@@ -134,23 +127,20 @@ class DependantTTestOfStudent implements \Countable
         return $this->int_count;
     }
 
-
-
     public function degreesOfFreedom()
     {
-        if(is_null($this->int_dof)){
+        if (is_null($this->int_dof)) {
             $this->compute();
         }
 
         return $this->int_dof;
     }
 
-
     public function dof()
     {
         return $this->degreesOfFreedom();
     }
-    
+
     public function sigma()
     {
         $this->compute();
@@ -158,8 +148,6 @@ class DependantTTestOfStudent implements \Countable
         return $this->float_sigma;
     }
 
-
-    
     public function t()
     {
         $this->compute();
@@ -167,16 +155,15 @@ class DependantTTestOfStudent implements \Countable
         return $this->float_t;
     }
 
-
     protected function compute()
     {
-        if(is_null($this->float_t)){
+        if (is_null($this->float_t)) {
             $this->int_count = count($this->arr_samples[0]);
             $this->int_dof = $this->int_count - 1;
 
             $d = new \Malenki\Math\Stats\Stats();
 
-            for($i = 0; $i < $this->int_count; $i++){
+            for ($i = 0; $i < $this->int_count; $i++) {
                 $d->add(
                     $this->arr_samples[0]->get($i)
                     -
@@ -186,14 +173,14 @@ class DependantTTestOfStudent implements \Countable
 
             $diff2 = new \Malenki\Math\Stats\Stats();
 
-            for($i = 0; $i < $this->int_count; $i++){
+            for ($i = 0; $i < $this->int_count; $i++) {
                 $diff2->add($d->get($i) - $d->mean);
             }
 
             $sigma_d = sqrt($diff2->sum2 / ($this->int_count - 1));
             $this->float_sigma = $sigma_d / sqrt($this->int_count);
 
-            if($sigma_d == 0){
+            if ($sigma_d == 0) {
                 throw new \LogicException('The given two samples are equals! Cannot compute T-Test!');
             }
             $this->float_t = $d->mean / $this->float_sigma;
